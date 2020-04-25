1b3939e2cbcf72c00878111e4e3c108f79e8ac4c
Géry Debongnie <ged@odoo.com>
2020-04-25 11:45:26 +0200

    wip (green)

--- src/____v2/owl2.ts
+++ src/____v2/owl2.ts
@@ -11,39 +11,49 @@ export class Component {
 // -----------------------------------------------------------------------------
 
 type Fn = (env, props) => FnInstance;
-type FnInstance = (props: any) => BlockTree;
+type FnInstance = (props: any) => Promise<VNode>;
 
 interface MountConfig {
   target: HTMLElement;
 }
 
-export async function mount(C: Fn | typeof Component | BlockTree, config: MountConfig) {
+type OwlElement = Fn | typeof Component | VNode;
+
+export async function mount(C: OwlElement, config: MountConfig): Promise<VNode> {
   // Build a Block
-  let tree: BlockTree;
+  let node: VNode;
   if (typeof C === "object") {
-    tree = C;
+    node = C;
   } else if (C.prototype instanceof Component) {
-    tree = buildFromComponent(C as typeof Component);
+    node = await buildFromComponent(C as typeof Component);
   } else {
-    tree = buildFromFn(C as Fn);
+    node = await buildFromFn(C as Fn);
   }
   // patch it to the target
-  patch(tree, config.target);
+  patch(node, config.target);
+  return node;
 }
 
-function buildFromComponent(C: typeof Component): BlockTree {
+function buildFromComponent(C: typeof Component): Promise<VNode> {
   let template: string = C.template;
   const c = new C();
   return qweb.render(template, c);
 }
 
-function buildFromFn(fn: Fn): BlockTree {
+function buildFromFn(fn: Fn): Promise<VNode> {
   const instance = fn({}, {});
   return instance({});
 }
 
-export function render(template: string, context: RenderContext = {}) {
-  return qweb.render(template, context);
+export function render(template: string, context?: RenderContext): Promise<VNode>;
+export function render(block: VNode, context?: RenderContext): Promise<VNode>;
+export function render(elem: string | VNode, context: RenderContext = {}): Promise<VNode> {
+  if (typeof elem === "string") {
+    return qweb.render(elem, context);
+  }
+  elem.context = context;
+  elem.builder.update(elem, context);
+  return Promise.resolve(elem);
 }
 
 // -----------------------------------------------------------------------------
@@ -55,37 +65,46 @@ interface RenderContext {
 }
 
 type RawTemplate = string;
-type CompiledTemplate = (context: RenderContext) => BlockTree;
+type CompiledTemplate = (context: RenderContext) => VNode;
 
-interface BuildingBlock {
+// todo: make this a class (??)
+interface Builder {
   id: number;
-  el: HTMLElement;
-  textNodes: Text[];
-  initialize(this: BuildingBlock, blockTree: BlockTree);
-  // update(blockTree: BlockTree, context)
+  elems: HTMLElement[];
+  fragments: DocumentFragment[];
+  texts: Text[];
+  initialize(this: Builder, node: VNode);
+  update(node: VNode, context);
 }
 
-interface BlockTree {
+class VNode {
   context: RenderContext;
-  blockId: number;
-  el: HTMLElement | null;
+  builder: Builder;
+  root: HTMLElement | DocumentFragment | Text | null = null;
+  elems: HTMLElement[] = [];
+  texts: Text[] = [];
+
+  constructor(builderId: number, context: RenderContext) {
+    this.builder = qweb.builders[builderId];
+    this.context = context;
+  }
 }
 
 interface QWeb {
   rawTemplates: { [name: string]: RawTemplate };
   templates: { [name: string]: CompiledTemplate };
-  blocks: { [id: number]: BuildingBlock };
+  builders: { [id: number]: Builder };
 
-  render(template: string, context?: RenderContext): BlockTree;
+  render(template: string, context?: RenderContext): Promise<VNode>;
   compile(template: string): CompiledTemplate;
 }
 
 const qweb: QWeb = {
   rawTemplates: {},
   templates: {},
-  blocks: {},
+  builders: {},
 
-  render(template: string, context: RenderContext = {}): BlockTree {
+  async render(template: string, context: RenderContext = {}): Promise<VNode> {
     let fn = qweb.templates[template];
     if (!fn) {
       fn = qweb.compile(template);
@@ -99,7 +118,7 @@ const qweb: QWeb = {
       throw new Error("BOOM");
     }
     for (let b of ct.blocks) {
-      qweb.blocks[b.id] = b;
+      qweb.builders[b.id] = b;
     }
     qweb.templates[template] = ct.fn;
     return ct.fn;
@@ -110,17 +129,16 @@ const qweb: QWeb = {
 // Tree Diff Engine
 // ----------------------------------------------------------------------------
 
-function patch(tree: BlockTree, target: HTMLElement) {
-  const block = qweb.blocks[tree.blockId];
-  block.initialize(tree);
-  target.appendChild(tree.el!);
+function patch(node: VNode, target: HTMLElement) {
+  node.builder.initialize(node);
+  target.appendChild(node.root!);
 }
 
 // -----------------------------------------------------------------------------
 // TEMP STUFF
 // -----------------------------------------------------------------------------
 interface CT {
-  blocks: BuildingBlock[];
+  blocks: Builder[];
   fn: CompiledTemplate;
 }
 const compiledTemplates: { [str: string]: CT } = {};
@@ -130,15 +148,17 @@ compiledTemplates["<div>simple block</div>"] = {
   blocks: [
     {
       id: 1,
-      el: makeEl("<div>simple block</div>"),
-      textNodes: [],
-      initialize(bt) {
-        bt.el = this.el.cloneNode(true) as HTMLElement;
+      elems: [makeEl("<div>simple block</div>")],
+      fragments: [],
+      texts: [],
+      initialize(node: VNode) {
+        node.root = this.elems[0].cloneNode(true) as HTMLElement;
       },
+      update() {},
     },
   ],
   fn: (context: RenderContext) => {
-    return { blockId: 1, context, el: null };
+    return new VNode(1, context);
   },
 };
 
@@ -147,15 +167,17 @@ compiledTemplates["<div>functional component</div>"] = {
   blocks: [
     {
       id: 2,
-      el: makeEl("<div>functional component</div>"),
-      textNodes: [],
-      initialize(bt) {
-        bt.el = this.el.cloneNode(true) as HTMLElement;
+      elems: [makeEl("<div>functional component</div>")],
+      fragments: [],
+      texts: [],
+      initialize(node: VNode) {
+        node.root = this.elems[0].cloneNode(true) as HTMLElement;
       },
+      update() {},
     },
   ],
   fn: (context: RenderContext) => {
-    return { blockId: 2, context, el: null };
+    return new VNode(2, context);
   },
 };
 
@@ -164,15 +186,17 @@ compiledTemplates["<div>class component</div>"] = {
   blocks: [
     {
       id: 3,
-      el: makeEl("<div>class component</div>"),
-      textNodes: [],
-      initialize(bt) {
-        bt.el = this.el.cloneNode(true) as HTMLElement;
+      elems: [makeEl("<div>class component</div>")],
+      fragments: [],
+      texts: [],
+      initialize(node: VNode) {
+        node.root = this.elems[0].cloneNode(true) as HTMLElement;
       },
+      update() {},
     },
   ],
   fn: (context: RenderContext) => {
-    return { blockId: 3, context, el: null };
+    return new VNode(3, context);
   },
 };
 
@@ -181,15 +205,66 @@ compiledTemplates['<div>Hello <t t-esc="name"/></div>'] = {
   blocks: [
     {
       id: 4,
-      el: makeEl("<div>Hello Alex</div>"),
-      textNodes: [],
-      initialize(bt) {
-        bt.el = this.el.cloneNode(true) as HTMLElement;
+      elems: [makeEl("<div>Hello </div>")],
+      fragments: [],
+      texts: [],
+      initialize(node: VNode) {
+        const context = node.context;
+        const el1 = this.elems[0].cloneNode(true) as HTMLElement;
+        const textNode1 = document.createTextNode(context.name);
+        el1.appendChild(textNode1);
+        node.texts.push(textNode1);
+        node.root = el1;
+      },
+      update(node: VNode, context: RenderContext) {
+        node.texts[0].textContent = context.name;
       },
     },
   ],
   fn: (context: RenderContext) => {
-    return { blockId: 4, context, el: null };
+    return new VNode(4, context);
+  },
+};
+
+// simple text node
+compiledTemplates["simple text node"] = {
+  blocks: [
+    {
+      id: 5,
+      elems: [],
+      fragments: [],
+      texts: [document.createTextNode("simple text node")],
+      initialize(node: VNode) {
+        node.root = this.texts[0].cloneNode(true) as Text;
+      },
+      update() {},
+    },
+  ],
+  fn: (context: RenderContext) => {
+    return new VNode(5, context);
+  },
+};
+
+// multi root template
+const f = document.createDocumentFragment();
+f.appendChild(makeEl("<div>a</div>"));
+f.appendChild(makeEl("<div>b</div>"));
+
+compiledTemplates["<div>a</div><div>b</div>"] = {
+  blocks: [
+    {
+      id: 6,
+      elems: [],
+      fragments: [f],
+      texts: [],
+      initialize(node: VNode) {
+        node.root = this.fragments[0].cloneNode(true) as DocumentFragment;
+      },
+      update() {},
+    },
+  ],
+  fn: (context: RenderContext) => {
+    return new VNode(6, context);
   },
 };
 
--- tests/____v2/owl2.test.ts
+++ tests/____v2/owl2.test.ts
@@ -1,4 +1,4 @@
-import { render, mount, Component } from "../../src/____v2/owl2";
+import { Component, mount, render } from "../../src/____v2/owl2";
 import { makeTestFixture } from "../helpers";
 
 let fixture: HTMLElement;
@@ -8,8 +8,11 @@ beforeEach(() => {
 });
 
 describe("owl2", () => {
+  // ---------------------------------------------------------------------------
+  // simple mounting operation, static content
+  // ---------------------------------------------------------------------------
   test("can mount a simple template", async () => {
-    const block = render(`<div>simple block</div>`);
+    const block = await render(`<div>simple block</div>`);
     await mount(block, { target: fixture });
     expect(fixture.innerHTML).toBe("<div>simple block</div>");
   });
@@ -32,8 +35,65 @@ describe("owl2", () => {
     expect(fixture.innerHTML).toBe("<div>class component</div>");
   });
 
+  // ---------------------------------------------------------------------------
+  // simple mounting operation, only a text node
+  // ---------------------------------------------------------------------------
+  test("just a template, text node", async () => {
+    const block = await render(`simple text node`);
+    await mount(block, { target: fixture });
+    expect(fixture.innerHTML).toBe("simple text node");
+  });
+
+  test("functional component, text node", async () => {
+    function Test() {
+      return () => render(`simple text node`);
+    }
+
+    await mount(Test, { target: fixture });
+    expect(fixture.innerHTML).toBe("simple text node");
+  });
+
+  test("class component, text node", async () => {
+    class Test extends Component {
+      static template = `simple text node`;
+    }
+
+    await mount(Test, { target: fixture });
+    expect(fixture.innerHTML).toBe("simple text node");
+  });
+
+  // ---------------------------------------------------------------------------
+  // simple mounting operation, multiroot content
+  // ---------------------------------------------------------------------------
+  test("simple template, multiroot", async () => {
+    const block = await render(`<div>a</div><div>b</div>`);
+    await mount(block, { target: fixture });
+    expect(fixture.innerHTML).toBe("<div>a</div><div>b</div>");
+  });
+
+  test("simple functional component, multiroot", async () => {
+    function Test() {
+      return () => render(`<div>a</div><div>b</div>`);
+    }
+
+    await mount(Test, { target: fixture });
+    expect(fixture.innerHTML).toBe("<div>a</div><div>b</div>");
+  });
+
+  test("simple class component, multiroot", async () => {
+    class Test extends Component {
+      static template = `<div>a</div><div>b</div>`;
+    }
+
+    await mount(Test, { target: fixture });
+    expect(fixture.innerHTML).toBe("<div>a</div><div>b</div>");
+  });
+
+  // ---------------------------------------------------------------------------
+  // simple mounting operation, dynamic content
+  // ---------------------------------------------------------------------------
   test("block with dynamic content", async () => {
-    const block = render(`<div>Hello <t t-esc="name"/></div>`, { name: "Alex" });
+    const block = await render(`<div>Hello <t t-esc="name"/></div>`, { name: "Alex" });
 
     await mount(block, { target: fixture });
     expect(fixture.innerHTML).toBe("<div>Hello Alex</div>");
@@ -56,5 +116,41 @@ describe("owl2", () => {
 
     await mount(Test, { target: fixture });
     expect(fixture.innerHTML).toBe("<div>Hello Alex</div>");
+  });
+
+  // ---------------------------------------------------------------------------
+  // mounting, then update (on dynamic content)
+  // ---------------------------------------------------------------------------
+  test("updating a block with dynamic content", async () => {
+    const block = await render(`<div>Hello <t t-esc="name"/></div>`, { name: "Alex" });
+
+    await mount(block, { target: fixture });
+    expect(fixture.innerHTML).toBe("<div>Hello Alex</div>");
+
+    await render(block, { name: "Lyra" });
+    expect(fixture.innerHTML).toBe("<div>Hello Lyra</div>");
+  });
+
+  test("updating a functional component with dynamic content", async () => {
+    function Test() {
+      return () => render(`<div>Hello <t t-esc="name"/></div>`, { name: "Alex" });
+    }
+    const block = await mount(Test, { target: fixture });
+    expect(fixture.innerHTML).toBe("<div>Hello Alex</div>");
+
+    await render(block, { name: "Lyra" });
+    expect(fixture.innerHTML).toBe("<div>Hello Lyra</div>");
+  });
+
+  test("updating a class component with dynamic content", async () => {
+    class Test extends Component {
+      name = "Alex";
+      static template = `<div>Hello <t t-esc="name"/></div>`;
+    }
+    const block = await mount(Test, { target: fixture });
+    expect(fixture.innerHTML).toBe("<div>Hello Alex</div>");
+
+    await render(block, { name: "Lyra" });
+    expect(fixture.innerHTML).toBe("<div>Hello Lyra</div>");
   });
 });
